bf40fd765cbd34e392cd4a689770668f01f09507
UostonDanilo <uostondanilo@hotmail.com>
2023-11-16 15:17:44 -0300

    meu segundo

--- Desafio.js
+++ Desafio.js
@@ -1,27 +1,29 @@
 let nomedoHeroi = "Danilo" 
-let xpdoHeroi = 33000
+let xpdoHeroi = 55000
 
 if (xpdoHeroi < 1000){
- console.log (niveldoHeroi = "Ferro") 
+  niveldoHeroi = "Ferro"
  
 }else if (xpdoHeroi >= 1001 && xpdoHeroi <= 2000){
- console.log (niveldoHeroi="Bronze") 
-
+  niveldoHeroi="Bronze"
+ 
 }else if (xpdoHeroi >= 2001 && xpdoHeroi <= 5000){
-console.log (niveldoHeroi = "Prata") 
+  niveldoHeroi = "Prata"
 
 }else if (xpdoHeroi >= 6001 && xpdoHeroi <= 7000){
-console.log (niveldoHeroi = "Ouro") 
+  niveldoHeroi = "Ouro"
 
 }else if (xpdoHeroi >= 7001 && xpdoHeroi <= 8000){
-console.log (niveldoHeroi = "Platina")
+  niveldoHeroi = "Platina"
 
 }else if (xpdoHeroi >= 8001 && xpdoHeroi <= 9000){
-console.log (niveldoHeroi = "Ascendente")
+  niveldoHeroi = "Ascendente"
 
 }else if (xpdoHeroi >= 9001 && xpdoHeroi <= 10000){
-console.log (niveldoHeroi = "Imortal")
+  niveldoHeroi = "Imortal"
 
 } else {
-console.log(niveldoHeroi = "Radiante") 
+  niveldoHeroi = "Radiante"
 }
+
+console.log("O Heroi de nome " + "Danilo" + " esta no nivel de " + niveldoHeroi)
