2096176fe3fa6702c65aeda8233c469dbb62d3fc
UostonDanilo <uostondanilo@hotmail.com>
2023-11-16 14:28:29 -0300

    first commit

---
+++ Desafio.js
@@ -0,0 +1,27 @@
+let nomedoHeroi = "Danilo" 
+let xpdoHeroi = 33000
+
+if (xpdoHeroi < 1000){
+ console.log (niveldoHeroi = "Ferro") 
+ 
+}else if (xpdoHeroi >= 1001 && xpdoHeroi <= 2000){
+ console.log (niveldoHeroi="Bronze") 
+
+}else if (xpdoHeroi >= 2001 && xpdoHeroi <= 5000){
+console.log (niveldoHeroi = "Prata") 
+
+}else if (xpdoHeroi >= 6001 && xpdoHeroi <= 7000){
+console.log (niveldoHeroi = "Ouro") 
+
+}else if (xpdoHeroi >= 7001 && xpdoHeroi <= 8000){
+console.log (niveldoHeroi = "Platina")
+
+}else if (xpdoHeroi >= 8001 && xpdoHeroi <= 9000){
+console.log (niveldoHeroi = "Ascendente")
+
+}else if (xpdoHeroi >= 9001 && xpdoHeroi <= 10000){
+console.log (niveldoHeroi = "Imortal")
+
+} else {
+console.log(niveldoHeroi = "Radiante") 
+}
